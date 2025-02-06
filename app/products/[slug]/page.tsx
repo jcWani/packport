d4f2459523123ce5a5daa22f822544557162fb80
jcWani <james.waniwan01@gmail.com>
2025-02-06 18:12:56 +0800

    Update params slug type

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -48,7 +48,11 @@ export async function generateStaticParams() {
   return [];
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
 
   const variant = await db.query.productVariants.findFirst({
